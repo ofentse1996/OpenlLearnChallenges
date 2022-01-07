4f943b8b101c4f24c61e76da8ea9858254a90310
ofentse1996 <54141988+ofentse1996@users.noreply.github.com>
2022-01-07 10:23:05 +0200

    Sorting Adding by One

---
+++ SortingAddByOne.java
@@ -0,0 +1,17 @@
+public class SortingAddByOne{
+
+    public static void main(String [] args){
+
+      int [] myIntArray = new int[50];
+
+        for (int i=0; i< myIntArray.length; i++){
+            myIntArray[i] = i+1;
+        }
+        for (int i=0; i< myIntArray.length; i++){
+            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+        }
+
+    }
+    }
+    
+    
