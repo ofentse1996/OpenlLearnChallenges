11ad55307c085ebf70058167dde374696fc173ee
ofentse1996 <54141988+ofentse1996@users.noreply.github.com>
2022-01-07 10:52:43 +0200

    Empty Array

---
+++ EmptyArray.java
@@ -0,0 +1,15 @@
+public class EmptyArray{
+
+    public static void main(String [] args){
+
+      int [] myIntArray = new int[50];
+
+        for (int i=0; i< myIntArray.length; i++){
+            myIntArray[i] = i+1;
+        }
+        for (int i=0; i< myIntArray.length; i++){
+            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+        }
+
+    }
+    }
