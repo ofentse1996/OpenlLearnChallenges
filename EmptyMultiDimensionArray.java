a83ae1917ef8fb08ba6198d7ec99a9ddc0d7df38
ofentse1996 <54141988+ofentse1996@users.noreply.github.com>
2022-01-07 11:18:42 +0200

    Empty Multi-Dimensional Array

---
+++ EmptyMultiDimensionArray.java
@@ -0,0 +1,19 @@
+public class EmptyMultiDimensionArray{
+
+    public static void main(String [] args){
+
+    //two dimensional array definition 
+    int [][] myIntArray = new int [100][30];
+    //printing the 2-d array
+    System.out.println("The two-dimensional array: ");
+    for (int i=0;i<myIntArray.length ;i++) {
+        for (int j=0;j<myIntArray[i].length ;j++ ) {
+            myIntArray[i][j] = j+1;
+            System.out.print(myIntArray[i][j] + " ");
+        }
+        System.out.println(" ");
+    }
+
+    }
+}
+
